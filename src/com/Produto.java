690973cfc5f53e0948ced8f395d5340c63b2f0a3
educanavarro <50561592+educanavarro@users.noreply.github.com>
2019-06-03 02:10:51 -0300

    Add files via upload

---
+++ src/com/Produto.java
@@ -0,0 +1,51 @@
+package com.company;
+
+public class Produto {
+    private String tipo;
+    private String fabricante;
+    private float preco;
+
+    public Produto() {
+    }
+
+    public Produto (String tipo, String fabricante, float preco) {
+        this.tipo = tipo;
+        this.fabricante = fabricante;
+        this.preco = preco;
+    }
+
+    public void ExibirInformacoes() {
+        ExibirInformacoes("Dados dos Produtos");
+    }
+
+    public void ExibirInformacoes(String cabecalho) {
+        System.out.println("--------------------------------------------------");
+        System.out.println(cabecalho);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Fabricante: " + this.getFabricante());
+        System.out.println("Preço: " + this.getPreco());
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+}
